0b16119a62cc87e278c3ba075583d0010e906ad8
Adgeh <belaybirhanu407@gmail.com>
2024-01-25 08:45:21 +0300

    behavioral pattern one time commit

--- stractural/FacadeDesignPattern/src/ShapeMaker.java
+++ stractural/FacadeDesignPattern/src/ShapeMaker.java
@@ -13,11 +13,11 @@ public class ShapeMaker {
    public void drawCircle(){
       circle.draw();
    }
-   
+
    public void drawRectangle(){
       rectangle.draw();
    }
-   
+
    public void drawSquare(){
       square.draw();
    }
